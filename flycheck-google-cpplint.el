862e4ee684a17797b28e4a21fbca021430fc7fe7
JenChieh <jcs090218@gmail.com>
2021-02-09 21:23:50 +0800

    Update commentary's url

--- flycheck-google-cpplint.el
+++ flycheck-google-cpplint.el
@@ -30,13 +30,13 @@
 
 ;; If you're want to write code according to the Google C++ Style Guide,
 ;; this will help a great deal.
-;; http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml
+;; https://google.github.io/styleguide/cppguide.html
 
 ;; I recommend that the package google-c-style also installed with.
-;; http://melpa.milkbox.net/#/google-c-style
+;; https://melpa.org/#/google-c-style
 
 ;; For more infomations, please check the GitHub
-;; https://github.com/senda-akiha/flycheck-google-cpplint/
+;; https://github.com/flycheck/flycheck-google-cpplint/
 
 ;;;; Setup
 
